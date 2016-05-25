d861ba1980321d3702fad0d1b59665e26db81cc5
jamesyburr <vint.james@gmail.com>
2016-05-25 16:36:34 -0400

    Added parsing error message, created method for options

--- src/com/james/Main.java
+++ src/com/james/Main.java
@@ -4,7 +4,7 @@ import jodd.json.JsonParser;
 import jodd.json.JsonSerializer;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -14,50 +14,39 @@ public class Main {
     public static void main(String[] args) {
         // create game list
         VideoGame vg = loadGame("gfile.json");
+
         if (vg == null) {
 
-
-            System.out.println("Enter game name:");
             Scanner scanner = new Scanner(System.in);
-            vg.name = scanner.nextLine();
-            System.out.println("Enter genre:");
-            vg.genre = scanner.nextLine();
-            System.out.println("Enter company:");
-            vg.company = scanner.nextLine();
-            System.out.println("Enter system:");
-            vg.system = scanner.nextLine();
-            System.out.println("Enter # of players:");
-            vg.players = Integer.valueOf(scanner.nextLine());
-            System.out.println("Enter year released");
-            vg.released = Integer.valueOf(scanner.nextLine());
-            saveGame(vg);
+            vg = new VideoGame();
+            options(vg, scanner);
+
         }
         else {
             System.out.println(vg);
-            System.out.println("Would you like to update info? [y/n}");
+            System.out.println("Update info:");
             Scanner scanner = new Scanner(System.in);
-            String choice = scanner.nextLine();
-            if (choice.equalsIgnoreCase("y")) {
-                System.out.println("Enter game name:");
-                vg.name = scanner.nextLine();
-                System.out.println("Enter genre:");
-                vg.genre = scanner.nextLine();
-                System.out.println("Enter company:");
-                vg.company = scanner.nextLine();
-                System.out.println("Enter system:");
-                vg.system = scanner.nextLine();
-                System.out.println("Enter # of players:");
-                vg.players = Integer.valueOf(scanner.nextLine());
-                System.out.println("Enter year released");
-                vg.released = Integer.valueOf(scanner.nextLine());
-                saveGame(vg);
-            }
-            else if (choice.equalsIgnoreCase("n")) {
-
-            }
+            options(vg, scanner);
         }
-
     }
+
+    public static void options (VideoGame vg, Scanner scanner) {
+        System.out.println("Enter game name:");
+        vg.name = scanner.nextLine();
+        System.out.println("Enter genre:");
+        vg.genre = scanner.nextLine();
+        System.out.println("Enter company:");
+        vg.company = scanner.nextLine();
+        System.out.println("Enter system:");
+        vg.system = scanner.nextLine();
+        System.out.println("Enter # of players:");
+        vg.players = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter year released");
+        vg.released = Integer.valueOf(scanner.nextLine());
+        saveGame(vg);
+    }
+
+
     //write file
     public static void saveGame(VideoGame vg) {
         JsonSerializer serializer = new JsonSerializer();
@@ -80,7 +69,8 @@ public class Main {
             String contents = scanner.next();
             JsonParser parser = new JsonParser();
             return parser.parse(contents, VideoGame.class);
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
+            System.out.println("Something went wrong with reading file");
         }
         return null;
     }
