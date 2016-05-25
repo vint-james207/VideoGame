43b1c06ac81877313f6a111d7deac7fa7fc8aba8
jamesyburr <vint.james@gmail.com>
2016-05-25 19:54:50 -0400

    committed assertTrue, with errors

--- Test/com/james/MainTest.java
+++ Test/com/james/MainTest.java
@@ -12,6 +12,14 @@ public class MainTest {
     public void testSaveAndLoad() {
         VideoGame testgame = new VideoGame ("TestName", "TestGenre", "TestCompany", "TestSystem", 2, 2000);
         Main.saveGame(testgame, "test.json");
-        Main.loadGame("test.json");
+
+        VideoGame testVideoGame = Main.loadGame("test.json");
+
+        assertTrue(testgame.name.equals(testVideoGame));
+        assertTrue(testgame.genre.equals(testVideoGame));
+        assertTrue(testgame.company.equals(testVideoGame));
+        assertTrue(testgame.system.equals(testVideoGame));
+        assertTrue(testgame == (testVideoGame));
+        assertTrue(testgame == (testVideoGame));
     }
 }
